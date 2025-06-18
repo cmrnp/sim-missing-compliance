e6b4fd90227b830c88cd196690bbb4a718df3c81
Cameron Patrick <cameronjpatrick@gmail.com>
2025-06-18 11:12:35 +1000

    add SMCFCS method

--- R/missingness-methods.R
+++ R/missingness-methods.R
@@ -71,6 +71,70 @@ missingness_mi_binary <- function(dat, estimators, method, m = 50, iter = 1) {
     reframe(run_estimator_mi(imp_all_df, estimator_fn))
 }
 
+# SMCFCS multiple imputation of binary compliance measure
+missingness_mi_smcfcs <- function(dat, estimators, method, m = 50, iter = 1) {
+  dat_for_imp <- dat %>%
+    select(trt, aux, confounder, dose_binary, outcome) %>%
+    mutate(dose_binary = as.factor(dose_binary))
+
+  # bodge: check to see if all participants group have the
+  # same dose (as binary variable) and if so, give up!
+  if (all(dat_for_imp$dose_binary == 0, na.rm = TRUE)) {
+    return(tibble())
+  }
+
+  # split data into control and treatment groups
+  # gotta be a data frame, not a tibble, for smcfcs()
+  dat_ctrl <- as.data.frame(filter(dat_for_imp, trt == 0))
+  dat_trt <- as.data.frame(filter(dat_for_imp, trt == 1))
+
+  # similar to above, if all treatment participants received the treatment,
+  # we can't do much
+  if (all(dat_trt$dose_binary == 1, na.rm = TRUE)) {
+    return(tibble())
+  }
+
+  # Impute treatment and control separately
+  # No missing data in control when we have missing data in compliance only
+  # so just repeat the complete dataset
+  imp_ctrl <-
+    list(impDatasets = rep(list(dat_ctrl), times = m))
+  # As above, only run smcfcs() if there's actually missing data or it will
+  # fail
+  if (any(is.na(dat_trt$dose_binary))) {
+    imp_trt <-
+      smcfcs(
+        originaldata = dat_trt,
+        smtype = "lm",
+        smformula = "outcome ~ dose_binary + confounder + aux",
+        method = c("", "", "", "logreg", ""),
+        m = m,
+        numit = iter
+      )
+  } else {
+    imp_trt <-
+      list(impDatasets = rep(list(dat_trt), times = m))
+  }
+  # Combine imputed treatment and control datasets
+  imp_all_df <-
+    bind_rows(
+      bind_rows(imp_ctrl$impDatasets, .id = ".imp"),
+      bind_rows(imp_trt$impDatasets, .id = ".imp")
+    ) %>%
+    mutate(.imp = as.integer(.imp)) %>%
+    group_by(.imp) %>%
+    mutate(.id = row_number()) %>%
+    ungroup() %>%
+    relocate(.imp, .id) %>%
+    arrange(.imp, .id) %>%
+    mutate(dose_binary = as.numeric(as.character(dose_binary)))
+
+  # Run estimators on imputed data
+  estimators %>%
+    rowwise(estimator_name) %>%
+    reframe(run_estimator_mi(imp_all_df, estimator_fn))
+}
+
 # Inverse probability weighting for missing data
 missingness_ipw <- function(dat, estimators) {
   dat <- dat %>%
@@ -96,5 +160,6 @@ missingness_methods <- tribble(
   "mi_logreg", \(d, e) missingness_mi_binary(d, e, method = "logreg"),
   "mi_logreg_boot", \(d, e) missingness_mi_binary(d, e, method = "logreg.boot"),
   "mi_pmm", \(d, e) missingness_mi_binary(d, e, method = "pmm"),
+  "smcfcs", missingness_mi_smcfcs,
   "ipw", missingness_ipw,
 )
