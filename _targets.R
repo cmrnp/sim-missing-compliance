d0cc0f7a367f35c11ce904446570a02342cc1ef3
Cameron Patrick <cameronjpatrick@gmail.com>
2025-06-18 19:42:25 +1000

    nanoparquet instead of parquet; cluster params changes

--- _targets.R
+++ _targets.R
@@ -5,7 +5,7 @@ source(here("functions.R"))
 # Set overall targets options
 tar_option_set(
   seed = 202411,
-  format = "parquet",
+  format = tar_format_nanoparquet(),
   error = "null",
 )
 
@@ -14,11 +14,11 @@ if (Sys.info()["nodename"] == "login001.meerkat.mcri.edu.au") {
   tar_option_set(
     controller = crew_controller_slurm(
       name = "misscompl",
-      workers = 320,
+      workers = 640,
       options_cluster = crew_options_slurm(
-        memory_gigabytes_required = 8,
+        memory_gigabytes_required = 3,
         time_minutes = 24 * 60,
-        partition = "prod_med",
+        partition = "prod_med,prod_long",
         script_lines = "
 #SBATCH --account=cebu
 module load r/4.4.1
@@ -40,19 +40,7 @@ module load r/4.4.1
 #     treatment_effect == "power80"
 #   )
 
-# Define targets
-list(
-  tar_target(
-    dgp_params,
-    get_dgp_params(scenario_list),
-    deployment = "main"
-  ),
-  tar_target(
-    scenario_params,
-    get_scenario_params(scenario_list, dgp_params),
-    deployment = "main"
-  ),
-
+sim_reps_target <-
   tar_map_rep(
     sim_reps,
     command =
@@ -63,7 +51,23 @@ list(
     batches = 100,
     reps = 100,
     combine = TRUE,
+  )
+# combining targets should take place on head node as it will exceed
+# RAM restrictions specified for batch jobs:
+sim_reps_target$combine$settings$deployment <- "main"
+
+# Define targets
+list(
+  tar_target(
+    dgp_params,
+    get_dgp_params(scenario_list)
   ),
+  tar_target(
+    scenario_params,
+    get_scenario_params(scenario_list, dgp_params)
+  ),
+
+  sim_reps_target,
 
   # Generate summary data frame of indiviudal replicate results
   tar_target(
